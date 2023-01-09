3caa42d85f9a7748a4ad1ce0ef29f89a277e1001
Syed Irtiza Hassan <irtiza.hassan@gmail.com>
2023-01-09 03:19:36 -0800

    new changes in panel_costing table and 'pamel_costing' sql file change from database.

--- panel_costing/cost/calculate/bus_bar.php
+++ panel_costing/cost/calculate/bus_bar.php
@@ -19,6 +19,61 @@ $bb_dimension_four = $_POST['bb_dimension_four'];
 $busbar_qty_four = $_POST['busbar_qty_four'];
 $busbar_weight_four = $_POST['busbar_weight_four'];
 $busbar_sleeve_four = $_POST['busbar_sleeve_four'];
+
+$bb_dimension_five = $_POST['bb_dimension_five'];
+$busbar_qty_five = $_POST['busbar_qty_five'];
+$busbar_weight_five = $_POST['busbar_weight_five'];
+$busbar_sleeve_five = $_POST['busbar_sleeve_five'];
+
+$bb_dimension_six = $_POST['bb_dimension_six'];
+$busbar_qty_six = $_POST['busbar_qty_six'];
+$busbar_weight_six = $_POST['busbar_weight_six'];
+$busbar_sleeve_six = $_POST['busbar_sleeve_six'];
+
+$bb_dimension_seven = $_POST['bb_dimension_seven'];
+$busbar_qty_seven = $_POST['busbar_qty_seven'];
+$busbar_weight_seven = $_POST['busbar_weight_seven'];
+$busbar_sleeve_seven = $_POST['busbar_sleeve_seven'];
+
+$bb_dimension_eight = $_POST['bb_dimension_eight'];
+$busbar_qty_eight = $_POST['busbar_qty_eight'];
+$busbar_weight_eight = $_POST['busbar_weight_eight'];
+$busbar_sleeve_eight = $_POST['busbar_sleeve_eight'];
+
+$bb_dimension_nine = $_POST['bb_dimension_nine'];
+$busbar_qty_nine = $_POST['busbar_qty_nine'];
+$busbar_weight_nine = $_POST['busbar_weight_nine'];
+$busbar_sleeve_nine = $_POST['busbar_sleeve_nine'];
+
+$bb_dimension_ten = $_POST['bb_dimension_ten'];
+$busbar_qty_ten = $_POST['busbar_qty_ten'];
+$busbar_weight_ten = $_POST['busbar_weight_ten'];
+$busbar_sleeve_ten = $_POST['busbar_sleeve_ten'];
+
+$bb_dimension_eleven = $_POST['bb_dimension_eleven'];
+$busbar_qty_eleven = $_POST['busbar_qty_eleven'];
+$busbar_weight_eleven = $_POST['busbar_weight_eleven'];
+$busbar_sleeve_eleven = $_POST['busbar_sleeve_eleven'];
+
+$bb_dimension_twelve = $_POST['bb_dimension_twelve'];
+$busbar_qty_twelve = $_POST['busbar_qty_twelve'];
+$busbar_weight_twelve = $_POST['busbar_weight_twelve'];
+$busbar_sleeve_twelve = $_POST['busbar_sleeve_twelve'];
+
+$bb_dimension_thirteen = $_POST['bb_dimension_thirteen'];
+$busbar_qty_thirteen = $_POST['busbar_qty_thirteen'];
+$busbar_weight_thirteen = $_POST['busbar_weight_thirteen'];
+$busbar_sleeve_thirteen = $_POST['busbar_sleeve_thirteen'];
+
+$bb_dimension_fourteen = $_POST['bb_dimension_fourteen'];
+$busbar_qty_fourteen = $_POST['busbar_qty_fourteen'];
+$busbar_weight_fourteen = $_POST['busbar_weight_fourteen'];
+$busbar_sleeve_fourteen = $_POST['busbar_sleeve_fourteen'];
+
+$bb_dimension_fifteen = $_POST['bb_dimension_fifteen'];
+$busbar_qty_fifteen = $_POST['busbar_qty_fifteen'];
+$busbar_weight_fifteen = $_POST['busbar_weight_fifteen'];
+$busbar_sleeve_fifteen = $_POST['busbar_sleeve_fifteen'];
 $id = $_POST['pc_id'];
 
 
@@ -35,6 +90,50 @@ if($busbar_weight_four == ""){
     $busbar_weight_four = 0;
 }
 
+if($busbar_weight_five == ""){
+    $busbar_weight_five = 0;
+}
+
+if($busbar_weight_six == ""){
+    $busbar_weight_six = 0;
+}
+
+if($busbar_weight_seven == ""){
+    $busbar_weight_seven = 0;
+}
+
+if($busbar_weight_eight == ""){
+    $busbar_weight_eight = 0;
+}
+
+if($busbar_weight_nine == ""){
+    $busbar_weight_nine = 0;
+}
+
+if($busbar_weight_ten == ""){
+    $busbar_weight_ten = 0;
+}
+
+if($busbar_weight_eleven == ""){
+    $busbar_weight_eleven = 0;
+}
+
+if($busbar_weight_twelve == ""){
+    $busbar_weight_twelve = 0;
+}
+
+if($busbar_weight_thirteen == ""){
+    $busbar_weight_thirteen = 0;
+}
+
+if($busbar_weight_fourteen == ""){
+    $busbar_weight_fourteen = 0;
+}
+
+if($busbar_weight_fifteen == ""){
+    $busbar_weight_fifteen = 0;
+}
+
 if($busbar_sleeve == ""){
     $busbar_sleeve = 0;
 }
@@ -47,6 +146,39 @@ if($busbar_sleeve_three == ""){
 if($busbar_sleeve_four == ""){
     $busbar_sleeve_four = 0;
 }
+if($busbar_sleeve_five == ""){
+    $busbar_sleeve_five = 0;
+}
+if($busbar_sleeve_six == ""){
+    $busbar_sleeve_six = 0;
+}
+if($busbar_sleeve_seven == ""){
+    $busbar_sleeve_seven = 0;
+}
+if($busbar_sleeve_eight == ""){
+    $busbar_sleeve_eight = 0;
+}
+if($busbar_sleeve_nine == ""){
+    $busbar_sleeve_nine = 0;
+}
+if($busbar_sleeve_ten == ""){
+    $busbar_sleeve_ten = 0;
+}
+if($busbar_sleeve_eleven == ""){
+    $busbar_sleeve_eleven = 0;
+}
+if($busbar_sleeve_twelve == ""){
+    $busbar_sleeve_twelve = 0;
+}
+if($busbar_sleeve_thirteen == ""){
+    $busbar_sleeve_thirteen = 0;
+}
+if($busbar_sleeve_fourteen == ""){
+    $busbar_sleeve_fourteen = 0;
+}
+if($busbar_sleeve_fifteen == ""){
+    $busbar_sleeve_fifteen = 0;
+}
 
 $SQL = 'SELECT * FROM pc_rate';
 $result = mysqli_query($conn, $SQL);
@@ -55,9 +187,13 @@ while ($panelCost = mysqli_fetch_array($result)) {
 }
 
 
-$weight_kg = $busbar_weight + $busbar_weight_two + $busbar_weight_three + $busbar_weight_four;
+$weight_kg = $busbar_weight + $busbar_weight_two + $busbar_weight_three + $busbar_weight_four + $busbar_weight_five + $busbar_weight_six +
+$busbar_weight_fifteen + $busbar_weight_seven + $busbar_weight_eight + $busbar_weight_nine + $busbar_weight_ten
++ $busbar_weight_eleven + $busbar_weight_twelve + $busbar_weight_thirteen + $busbar_weight_fourteen;
 $total_cost = $weight_kg*$bus_bar;
-$sleeve_cost = $busbar_sleeve + $busbar_sleeve_two + $busbar_sleeve_three + $busbar_sleeve_four;
+$sleeve_cost = $busbar_sleeve + $busbar_sleeve_two + $busbar_sleeve_three +  $busbar_sleeve_four + $busbar_sleeve_five
++ $busbar_sleeve_six + $busbar_sleeve_seven + $busbar_sleeve_eight + $busbar_sleeve_nine + $busbar_sleeve_ten
++ $busbar_sleeve_eleven + $busbar_sleeve_twelve + $busbar_sleeve_thirteen + $busbar_sleeve_fourteen + $busbar_sleeve_fifteen;
 
 $bbr_sleeve = $sleeve_cost + $total_cost;
 $sql ="UPDATE pc_cash_demand SET bbr_sleeve_budget = '".$bbr_sleeve."' WHERE pc_id = '".$id."'";
---
+++ panel_costing/cost/dimensions/bb_dimension_thirteen.php
@@ -0,0 +1,19 @@
+<?php
+require_once './config.php';
+$w = $_POST['door8_d'];
+$id=$_POST['pc_id'];
+
+session_start();
+date_default_timezone_set("Asia/Karachi");
+  $date = date('d-m-y h:i:s');
+  $sql = "INSERT INTO panelcostingmodifications(panel_id,updateDate,pc_description)VALUES('$id','$date','Updated Bus Bar 13 Dimension by ".$_SESSION['UsersRealName']." at')";
+  mysqli_query($conn, $sql);
+
+
+$sql ="UPDATE panel_costing SET bbr13_dimension = '".$w."'  WHERE id = '".$id."'";
+if (mysqli_query($conn, $sql)) {
+}
+else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+?>
