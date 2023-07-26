b20b0bf082f64dc636df0acd5c1fbd3da1f4fa8e
irtiza <irtiza.hassan@compresol.com>
2023-07-27 02:57:59 +0500

    zero non zero filter added to supplier balance sheet

--- reports/balance/suppbalance/SuppBalanceSheet.php
+++ reports/balance/suppbalance/SuppBalanceSheet.php
@@ -1,23 +1,17 @@
 <?php 
-	
-	$PathPrefix='../../../';
-
+	$PathPrefix = '../../../';
 	include('../../../includes/session.inc');
 	include('../../../includes/SQL_CommonFunctions.inc');
-	if(!userHasPermission($db, 'Supp_Balance_Sheet')) {
-
-			
-			header("Location: /sahamid/v2/reportLinks.php");
-			exit;
-		}
-
+	if (!userHasPermission($db, 'Supp_Balance_Sheet')) {
+		header("Location: /sahamid/v2/reportLinks.php");
+		exit;
+	}
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
-
 	<title>Supplier Balance</title>
-
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css">
@@ -36,22 +30,22 @@
 	<script src="../../../quotation/assets/vendor/modernizr/modernizr.js"></script>
 
 	<style>
-		th{
+		th {
 			text-align: center;
 		}
-		td{
+		td {
 			text-align: center;
 			vertical-align: middle !important;
 		}
-		.dataTables_filter label{
+		.dataTables_filter label {
 			width: 100% !important;
 		}
 
-		.dataTables_filter input{
+		.dataTables_filter input {
 		    border: 1px #ccc solid;
 			border-radius: 5px;
 		}
-		.textLeft{
+		.textLeft {
 			text-align: left;
 		}
 	</style>
@@ -59,7 +53,6 @@
 	<script>
 		var datatable = null;
 	</script>
-
 </head>
 <body>
 	<header style="font-size:1em; padding:7px; width:100%; background:#424242;">
@@ -77,26 +70,34 @@
   	</header>
 
   	<div class="col-md-12" style="text-align: center; margin-top: 20px">
+  		<div class="col-md-4 col-md-offset-4" style="margin-bottom: 20px">
+  			<label>Show Amount Due:</label>
+  			<select id="amountDueFilter" class="form-control">
+  				<option value="all">All</option>
+  				<option value="nonZero">Non-Zero</option>
+  				<option value="zero">Zero</option>
+  			</select>
+  		</div>
   		<table class="table table-bordered table-striped mb-none" id="datatable">
   			<thead>
   				<tr style="background-color: #424242; color: white">
-				  <th>Supplier #</th>
+					<th>Supplier #</th>
   					<th style="width: 1%; white-space: nowrap;">Supplier #</th>
   					<th style="text-align: left;">Supplier Name</th>
+					<th>Amount Due</th>
 					<th>Amount Due</th>
   					<th></th>
 					<th>Ledger Status</th>
 					<th>Ledger Status</th>
   				</tr>
   			</thead>
-  			<tbody>
-  				
-  			</tbody>
+  			<tbody></tbody>
   			<tfoot>
   				<tr style="background-color: #424242; color: white">
-				  <th>Supplier #</th>
+					<th>Supplier #</th>
   					<th style="width: 1%; white-space: nowrap;">Supplier #</th>
   					<th style="text-align: left;">Supplier Name</th>
+					<th>Amount Due</th>
   					<th>Amount Due</th>
   					<th></th>
 					<th>Ledger Status</th>
@@ -104,17 +105,12 @@
   				</tr>
   			</tfoot>
   		</table>
-  		
   	</div>
 	
 	<div class="col-md-12">
-	
 		<div class="btn-danger" style="text-align:center; padding:20px; margin:20px 0; margin-bottom: 50px">
-		
 			<h3 style='margin:0; padding:0; font-variant: petite-caps;'><i class='fa fa-money' aria-hidden='true'></i> Total Balance Payable: <span id="totalBalance">Calculating Balance...</span> <span style="font-size:1.5rem">PKR</span></h3>
-		
 		</div>
-	
 	</div>
 
   	<footer style="background:#424242; position:fixed; bottom:0; width:100%; text-align:center; z-index:150; padding: 5px">
@@ -127,203 +123,181 @@
 	<script src="../../../quotation/assets/vendor/sweetalert/sweetalert.min.js"></script>
 
 	<script>
-		
-	
-function updateDatabaseValue(table, column, supplierId, newValue) {
-    // Make an API call to update the value in the database
-    // Replace 'your_api_endpoint' with the actual API endpoint URL
-	console.log(supplierId);
-	$.ajax({
-        url: "<?php echo $RootPath; ?>"+"/ledgerStatusUpdateAPI.php",
-        method: 'POST',
-		
-    	
-        data: {
-            table,
-            column,
-            supplierId,
-            newValue
-        },
+	function updateDatabaseValue(table, column, supplierId, newValue) {
+		// Make an API call to update the value in the database
+		// Replace 'your_api_endpoint' with the actual API endpoint URL
+		console.log(supplierId);
+		$.ajax({
+			url: "<?php echo $RootPath; ?>"+"/ledgerStatusUpdateAPI.php",
+			method: 'POST',
+			data: {
+				table,
+				column,
+				supplierId,
+				newValue
+			},
+			success: function(data) {
+				// Handle the API response if required
+				console.log(data);
+			},
+			error: function(error) {
+				// Handle any errors that occur during the API call
+				console.error('Error:', error);
+			}
+		});
+	}
 
-        success: function(data) {
-            // Handle the API response if required
-            console.log(data);
-        },
-        error: function(error) {
-            // Handle any errors that occur during the API call
-            console.error('Error:', error);
-        }
-    });
-}
+	function updateRowTextColorAndCellBackground(selectElement) {
+		const selectedText = $(selectElement).find('option:selected').text();
+		const columnIndex = $(selectElement).closest('td').index();
+		const table = $(selectElement).closest('table').DataTable();
 
-function updateRowTextColorAndCellBackground(selectElement) {
-    const selectedText = $(selectElement).find('option:selected').text();
-    const columnIndex = $(selectElement).closest('td').index();
-    const table = $(selectElement).closest('table').DataTable();
+		table.column(columnIndex).nodes().each(function(cell, index) {
+			const cellText = $(cell).find('.ledgerstatus').find('option:selected').text();
+			if (cellText.includes('mismanaged')) {
+				$(cell).css('background-color', 'red');
+			} else if (cellText.includes('corrected')) {
+				$(cell).css('background-color', 'green');
+			} else {
+				$(cell).css('background-color', '');
+			}
+		});
 
-    table.column(columnIndex).nodes().each(function(cell, index) {
-        const cellText = $(cell).find('.ledgerstatus').find('option:selected').text();
-        if (cellText.includes('mismanaged')) {
-            $(cell).css('background-color', 'red');
-        } else if (cellText.includes('corrected')) {
-            $(cell).css('background-color', 'green');
-        } else {
-            $(cell).css('background-color', '');
-        }
-    });
+		// Change the text color of the entire row to yellow
+		const row = $(selectElement).closest('tr');
+		if (selectedText.includes('mismanaged') || selectedText.includes('corrected')) {
+			row.css('color', 'black');
+		} else {
+			row.css('color', ''); // Reset to default text color
+		}
+		// Update text in the second last column to be the same as the selected value in the last column
+		const secondLastColumnIndex = table.column(columnIndex - 1).index();
+		table.column(secondLastColumnIndex).nodes().each(function(cell, index) {
+			const lastColumnValue = $(cell).next().find('.ledgerstatus').val();
+			$(cell).text(lastColumnValue);
+		});
+	}
 
-    // Change the text color of the entire row to yellow
-    const row = $(selectElement).closest('tr');
-    if (selectedText.includes('mismanaged') || selectedText.includes('corrected')) {
-        row.css('color', 'black');
-    } else {
-        row.css('color', ''); // Reset to default text color
-    }
-	 // Update text in the second last column to be the same as the selected value in the last column
-	 const secondLastColumnIndex = table.column(columnIndex - 1).index();
-		        table.column(secondLastColumnIndex).nodes().each(function(cell, index) {
-		            const lastColumnValue = $(cell).next().find('.ledgerstatus').val();
-		            $(cell).text(lastColumnValue);
-		        });
-}
+	(function($) {
+		'use strict';
 
+		var datatable;
 
-(function($) {
-    'use strict';
+		var datatableInit = function() {
+			datatable = $('#datatable').DataTable({
+				"aoColumnDefs": [
+					{ "sClass": "textLeft", "aTargets": [ 1 ] },
+					{ "searchable": false, "targets": [ 7 ] },
+					{ "visible": false, "targets": [ 0, 3, 6 ] }
+				],
+				dom: 'lBftip',
+				buttons: [
+					{
+						extend: 'csv',
+						text: 'Export CSV',
+						exportOptions: {
+							columns: [ 0, 2, 3, 6 ] // Include only the 1st, 3rd, and 5th column (zero-based index)
+						},
+						customizeData: function (csv) {
+							// Manually append the data from the hidden 4th column to the CSV
+							var data = datatable.rows().data();
+							for (var i = 0; i < data.length; i++) {
+								csv += '\n"' + data[i][5] + '"';
+							}
+							return csv;
+						}
+					}
+				],
+				"lengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]],
+				language: {
+					search: "_INPUT_",
+					searchPlaceholder: "Search..."
+				},
+				drawCallback: function () {
+					var api = this.api();
+					// $( api.table().footer() ).html(
+					//     '<tr style="background-color:#424242; color:white">'+
+					//     '<th>--</th><th>--</th><th>--</th>'+
+					//     '<th>'+api.column(3,{search:'applied'}).data().sum()+'</th>'+
+					//     '<th>'+api.column(4,{search:'applied'}).data().sum()+'</th>'+
+					//     '<th>'+api.column(5,{search:'applied'}).data().sum()+'</th>'+
+					//     '<th>'+api.column(6,{search:'applied'}).data().sum()+'</th>'+
+					//     '<th>'+api.column(7,{search:'applied'}).data().sum()+'</th></tr>'
+					// );
+				}
+			});
 
-    var datatable;
+			$('#datatable_length').find("label").html("<h3 style='margin:0; padding:0; font-variant: petite-caps;'><i class='fa fa-money' aria-hidden='true'></i> Supplier Balance Sheet <a class='btn btn-warning' href='../../../SupplierAllocations.php' target='_blank'>Allocate Balance</a></h3>");
+		};
 
-    var datatableInit = function() {
-        datatable = $('#datatable').DataTable({
-            "aoColumnDefs": [
-                { "sClass": "textLeft", "aTargets": [ 1 ] },
-				{ "searchable": false, "targets": [6] },
-				{ "visible": false, "targets": [0,5] }
-            ],
-			dom: 'lBftip',
-			buttons: [
-		                {
-		                    extend: 'csv',
-		                    text: 'Export CSV',
-							exportOptions: {
-		                        columns: [0, 2, 3,5] // Include only the 1st, 3rd, and 5th column (zero-based index)
-		                    },
-							customizeData: function (csv) {
-		                        // Manually append the data from the hidden 4th column to the CSV
-		                        var data = datatable.rows().data();
-		                        for (var i = 0; i < data.length; i++) {
-		                            csv += '\n"' + data[i][4] + '"';
-		                        }
-		                        return csv;
-		                    }
-		                }
-		            ],
+		$(function() {
+			datatableInit();
+			$("tbody tr td").html("Loading...");
 
-            "lengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]],
-            language: {
-                search: "_INPUT_",
-                searchPlaceholder: "Search..."
-            },
-            drawCallback: function () {
-                var api = this.api();
-                // $( api.table().footer() ).html(
-                //     '<tr style="background-color:#424242; color:white">'+
-                //     '<th>--</th><th>--</th><th>--</th>'+
-                //     '<th>'+api.column(3,{search:'applied'}).data().sum()+'</th>'+
-                //     '<th>'+api.column(4,{search:'applied'}).data().sum()+'</th>'+
-                //     '<th>'+api.column(5,{search:'applied'}).data().sum()+'</th>'+
-                //     '<th>'+api.column(6,{search:'applied'}).data().sum()+'</th>'+
-                //     '<th>'+api.column(7,{search:'applied'}).data().sum()+'</th></tr>'
-                // );
-            }
-        });
-		
+			$.ajax({
+				type: 'GET',
+				url: "<?php echo $RootPath; ?>"+"/SuppBalanceSheetAPI.php",
+				dataType: "json",
+				success: function(response) { 
+					datatable.rows.add(response).draw(false);
+				},
+				error: function(){
+					$("tbody tr td").html("Error...");
+				}
+			});
 
-        $('#datatable_length').find("label").html("<h3 style='margin:0; padding:0; font-variant: petite-caps;'><i class='fa fa-money' aria-hidden='true'></i> Supplier Balance Sheet <a class='btn btn-warning' href='../../../SupplierAllocations.php' target='_blank'>Allocate Balance</a></h3>");
-		
-    const columnIndex =  datatable.column('.ledgerstatus').index(); // Get the index of the "ledgerstatus" column
-	datatable.column(columnIndex).nodes().each(function(cell, index) {
-        const cellText = $(cell).find('.ledgerstatus').find('option:selected').text();
-        if (cellText.includes('mismanaged')) {
-            $(cell).css('background-color', 'red');
-            const row = table.row(index).node();
-            $(row).css('color', 'yellow');
-        } else if (cellText.includes('corrected')) {
-            $(cell).css('background-color', 'green');
-        } else {
-            $(cell).css('background-color', '');
-        }
-    });
-	};
+			$.ajax({
+				type: 'GET',
+				url: "<?php echo $RootPath; ?>"+"/TotalSuppBalanceAPI.php",
+				dataType: "json",
+				success: function(response) { 
+					$("#totalBalance").html(response.totalBalance);
+				},
+				error: function(){
+					$("#totalBalance").html("-Fetch Failed-");
+				}
+			});
 
-    $(function() {
-        datatableInit();
-        $("tbody tr td").html("Loading...");
+			$(document).on('change', '.ledgerstatus', function() {
+				const selectElement = this;
+				const table = $(selectElement).data('tablename');
+				const column = $(selectElement).data('colname');
+				const supplierId = $(selectElement).data('supplierid');
+				const newValue = $(selectElement).val();
 
-        $.ajax({
-            type: 'GET',
-            url: "<?php echo $RootPath; ?>"+"/SuppBalanceSheetAPI.php",
-            dataType: "json",
-            success: function(response) { 
-                datatable.rows.add(response).draw(false);
-            },
-            error: function(){
-                $("tbody tr td").html("Error...");
-            }
-        });
-		
+				updateDatabaseValue(table, column, supplierId, newValue);
 
+				// Call the function to change cell background color and row text color based on the selected value
+				updateRowTextColorAndCellBackground(selectElement);
+			});
 
-        $.ajax({
-            type: 'GET',
-            url: "<?php echo $RootPath; ?>"+"/TotalSuppBalanceAPI.php",
-            dataType: "json",
-            success: function(response) { 
-                $("#totalBalance").html(response.totalBalance);
-            },
-            error: function(){
-                $("#totalBalance").html("-Fetch Failed-");
-            }
-        });
-		
-		
-        $(document).on('change', '.ledgerstatus', function() {
-            const selectElement = this;
-            const table = $(selectElement).data('tablename');
-            const column = $(selectElement).data('colname');
-            const supplierId = $(selectElement).data('supplierid');
-            const newValue = $(selectElement).val();
+			$(document).on('mouseover', '.dataTable', function() {
+				const selectElement = this;
+				const table = $(selectElement).data('tablename');
+				const column = $(selectElement).data('colname');
+				const supplierId = $(selectElement).data('supplierid');
+				const newValue = $(selectElement).val();
 
-          
-    
-            updateDatabaseValue(table, column, supplierId, newValue);
+				updateDatabaseValue(table, column, supplierId, newValue);
 
-            // Call the function to change cell background color and row text color based on the selected value
-            updateRowTextColorAndCellBackground(selectElement);
-        });
-		$(document).on('mouseover', '.dataTable', function() {
-            const selectElement = this;
-            const table = $(selectElement).data('tablename');
-            const column = $(selectElement).data('colname');
-            const supplierId = $(selectElement).data('supplierid');
-            const newValue = $(selectElement).val();
+				// Call the function to change cell background color and row text color based on the selected value
+				updateRowTextColorAndCellBackground(selectElement);
+			});
 
-          
-    
-            updateDatabaseValue(table, column, supplierId, newValue);
+			// Filter the table based on the selected option in the "Amount Due" filter
+			$('#amountDueFilter').on('change', function() {
+				const filterValue = $(this).val();
 
-            // Call the function to change cell background color and row text color based on the selected value
-            updateRowTextColorAndCellBackground(selectElement);
-        });
-		
-    });
-
-})(jQuery);
-
-</script>
-
-	
-
-
+				if (filterValue === 'all') {
+					datatable.columns(3).search('').draw();
+				} else if (filterValue === 'nonZero') {
+					datatable.columns(3).search('[1-9]', true, false).draw();
+				} else if (filterValue === 'zero') {
+					datatable.columns(3).search('0', true, false).draw();
+				}
+			});
+		});
+	})(jQuery);
+	</script>
 </body>
 </html>
--- reports/balance/suppbalance/SuppBalanceSheetAPI.php
+++ reports/balance/suppbalance/SuppBalanceSheetAPI.php
@@ -1,52 +1,59 @@
 <?php
 
-	$PathPrefix='../../../';
+$PathPrefix = '../../../';
+include('../../../includes/session.inc');
+include('../../../includes/SQL_CommonFunctions.inc');
 
-	include('../../../includes/session.inc');
-	include('../../../includes/SQL_CommonFunctions.inc');
+$SQL = 'SELECT * FROM suppliers';
+$result = mysqli_query($db, $SQL);
 
-	$SQL = 'SELECT suppliers.supplierid as debtorno,suppliers.suppname as name,
-	suppliers.ledgerstatus as ledgerstatus,
-				SUM(ovamount - alloc) as curr  
-			FROM supptrans 
-			INNER JOIN suppliers ON suppliers.supplierid = supptrans.supplierno 
-			WHERE supptrans.type=601
-			AND supptrans.settled=0
-			AND supptrans.reversed=0
-			GROUP BY suppliers.supplierid'; 
-	
-	$result = mysqli_query($db, $SQL);
-	
-	$response = [];
+$response = [];
 
-	while($customer = mysqli_fetch_assoc($result)){
+// Assuming you have already fetched the supplier data and stored it in $result.
 
-		$res = [];
+while ($customer = mysqli_fetch_assoc($result)) {
+    $res = [];
 
-		$res[0] = $customer['debtorno'];
-		$res[1] = "<form action='".$RootPath."/../suppstatement/SupplierStatement.php' method='post' target='_blank'>
-						<input type='hidden' name='FormID' value='".$_SESSION['FormID']."' />
-						<input type='hidden' name='cust' value='".$customer['debtorno']."' />
-						<input type='submit' class='btn btn-info' style='width:100%' value='".$customer['debtorno']."' />
-					</form>";
-		
-		$res[2] = $customer['name'];
-		$res[3] = locale_number_format($customer['curr'],2). "<sub>PKR</sub>";
-		$res[4] = '<a href="../../../Payments.php?SupplierID='.$customer['debtorno'].'" class="btn btn-info" target="_blank" style="font-size:11px; white-space: nowrap;">Enter Payment</a>';
-		$selectMenu = '<select name="ledgerstatus" class="form-control ledgerstatus" data-tablename="suppliers" data-colname="ledgerstatus" data-supplierid='.$customer['debtorno'].'>
-                      <option value="showroom mismanaged" ' . ($customer['ledgerstatus'] === 'showroom mismanaged' ? 'selected' : '') . '>showroom mismanaged</option>
-                      <option value="showroom corrected" ' . ($customer['ledgerstatus'] === 'showroom corrected' ? 'selected' : '') . '>showroom corrected</option>
-                      <option value="mto mismanaged" ' . ($customer['ledgerstatus'] === 'mto mismanaged' ? 'selected' : '') . '>mto mismanaged</option>
-                      <option value="mto corrected" ' . ($customer['ledgerstatus'] === 'mto corrected' ? 'selected' : '') . '>mto corrected</option>
-                  </select>';
-   // $res[4] = $selectMenu;
-   $res[5] = $customer['ledgerstatus'];
-   $res[6] = $selectMenu;
-	
-		$response[] = $res;
+    $res[0] = $customer['supplierid'];
+    $res[1] = "<form action='" . $RootPath . "/../suppstatement/SupplierStatement.php' method='post' target='_blank'>
+                    <input type='hidden' name='FormID' value='" . $_SESSION['FormID'] . "' />
+                    <input type='hidden' name='cust' value='" . $customer['supplierid'] . "' />
+                    <input type='submit' class='btn btn-info' style='width:100%' value='" . $customer['supplierid'] . "' />
+                </form>";
 
-	}
+    $res[2] = $customer['suppname'];
+    
+    // New query to fetch the value of "curr" where available
+    $SQL = 'SELECT suppliers.supplierid as debtorno,
+                SUM(ovamount - alloc) as curr  
+            FROM supptrans 
+            LEFT OUTER JOIN suppliers ON suppliers.supplierid = supptrans.supplierno 
+            WHERE supptrans.type = 601
+            AND supptrans.settled = 0
+            AND supptrans.reversed = 0
+            AND suppliers.supplierid = "' . $customer['supplierid'] . '"
+            GROUP BY suppliers.supplierid'; 
 
-	echo json_encode($response);
-	return;
+    $result_curr = mysqli_query($db, $SQL);
+    $row_curr = mysqli_fetch_assoc($result_curr);
+    $res[3] = $row_curr['curr'] ?? 0; // If "curr" is not available, set it to 0.
+    $res[4] = locale_number_format($res[3], 2) . "<sub>PKR</sub>";
+    
+    $res[5] = '<a href="../../../Payments.php?SupplierID=' . $customer['supplierid'] . '" class="btn btn-info" target="_blank" style="font-size:11px; white-space: nowrap;">Enter Payment</a>';
+    
+    $selectMenu = '<select name="ledgerstatus" class="form-control ledgerstatus" data-tablename="suppliers" data-colname="ledgerstatus" data-supplierid=' . $customer['supplierid'] . '>
+                  <option value="showroom mismanaged" ' . ($customer['ledgerstatus'] === 'showroom mismanaged' ? 'selected' : '') . '>showroom mismanaged</option>
+                  <option value="showroom corrected" ' . ($customer['ledgerstatus'] === 'showroom corrected' ? 'selected' : '') . '>showroom corrected</option>
+                  <option value="mto mismanaged" ' . ($customer['ledgerstatus'] === 'mto mismanaged' ? 'selected' : '') . '>mto mismanaged</option>
+                  <option value="mto corrected" ' . ($customer['ledgerstatus'] === 'mto corrected' ? 'selected' : '') . '>mto corrected</option>
+              </select>';
+    // $res[4] = $selectMenu;
+    $res[6] = $customer['ledgerstatus'];
+    $res[7] = $selectMenu;
+
+    $response[] = $res;
+}
+
+echo json_encode($response);
+return;
 ?>
