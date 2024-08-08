d17b31cda6b4cf44e41b84be55c61d979b3cd3ec
Syed Irtiza Hassan <irtiza.hassan@gmail.com>
2024-08-08 10:56:31 +0500

    dcattachment list previous mathod replacing

--- v2/dcattachments.php
+++ v2/dcattachments.php
@@ -10,94 +10,258 @@ if (!userHasPermission($db, "show_dc_attachments_view")) {
     return;
 }
 
-
 if (isset($_POST['to'])) {
-    $from = $_POST['from'];
-    $to = $_POST['to'];
-    $userId = $_SESSION['UserID'];
-    $realName = $_SESSION['UsersRealName'];
-    $rootPath = "../" . $_SESSION['part_pics_dir'] . '/';
 
-    // Build SQL query based on permissions
-    $permissionCondition = userHasPermission($db, "show_all_dcs_attachments")
-        ? ""
-        : ' AND dcs.orderno IN (
-                SELECT dcs.orderno 
-                FROM dcs 
-                INNER JOIN salescase ON dcs.salescaseref = salescase.salescaseref
-                INNER JOIN salesman ON salescase.salesman = salesman.salesmanname
-                WHERE salesman.salesmanname = "' . $realName . '"
-                OR salesman.salesmancode IN (
-                    SELECT salescase_permissions.user 
-                    FROM salescase_permissions
-                    WHERE salescase_permissions.can_access = "' . $userId . '"
-                )
-            )';
+    $from     = $_POST['from'];
+    $to     = $_POST['to'];
+    $SQL1 = "SELECT DISTINCT dcno as dcno FROM grb";
+    $res1 = mysqli_query($db, $SQL1);
+    $grbarray = [];
+    while ($row1 = mysqli_fetch_assoc($res1)) {
+        $grbarray[$row1['dcno']] = "";
+    }
+    $SQLgrb = "SELECT * FROM grb";
+    $resgrb = mysqli_query($db, $SQLgrb);
 
-    $sqlDcs = 'SELECT dcs.orderno AS dcno, dcs.orddate AS date, dcs.salescaseref, debtorsmaster.name AS client, 
-                       debtorsmaster.dba, dcs.invoicegroupid, dcs.dcstatus
-               FROM dcs
-               INNER JOIN debtorsmaster ON debtorsmaster.debtorno = dcs.debtorno
-               WHERE dcs.orddate >= "' . $from . '" AND dcs.orddate <= "' . $to . '"' . $permissionCondition;
+    while ($rowgrb = mysqli_fetch_assoc($resgrb)) {
+        $grbarray[$rowgrb['dcno']] .=
+            '<a target="_blank" href = "../PDFGRB.php?grbno=' . $rowgrb['orderno'] . '">' . $rowgrb['orderno'] . '</a>' .
+            "<br/>";
+    }
+    $SQL2 = "SELECT distinct dcs.orderno from dcs inner join invoice on dcs.invoicegroupid=invoice.groupid";
+    $res2 = mysqli_query($db, $SQL2);
+    $invoicearray = [];
+    while ($row2 = mysqli_fetch_assoc($res2)) {
+        $invoicearray[$row2['orderno']] = "";
+    }
+    $SQLinvoice = "SELECT * from dcs inner join invoice on dcs.invoicegroupid=invoice.groupid  ";
+    $resinvoice = mysqli_query($db, $SQLinvoice);
 
-    $resDcs = mysqli_query($db, $sqlDcs);
+    while ($rowinvoice = mysqli_fetch_assoc($resinvoice)) {
 
-    $response = [];
-    $fileTypes = ['PO', 'CourierSlip', 'GRB', 'Invoice', 'CommercialInvoice'];
-    $filePaths = [];
-
-    foreach ($fileTypes as $type) {
-        $filePaths[$type] = glob($rootPath . $type . '_*.pdf');
+        $invoicearray[$rowinvoice['orderno']] .=
+            '<a target="_blank" href = "../PDFInvoice.php?InvoiceNo=' . $rowinvoice['invoiceno'] . '">' . $rowinvoice['invoiceno'] . '</a>' .
+            "<br/>";
     }
 
-    while ($rowDcs = mysqli_fetch_assoc($resDcs)) {
-        $dcno = $rowDcs['dcno'];
-        $attachments = [];
 
-        foreach ($fileTypes as $type) {
-            $attachments[$type] = [];
-            foreach ($filePaths[$type] as $file) {
-                if (strpos($file, $dcno) !== false) {
-                    $isDeleted = strpos($file, 'deleted') !== false;
-                    $attachments[$type][] = [
-                        'url' => $rootPath . basename($file),
-                        'deleted' => $isDeleted
-                    ];
-                }
+
+    if (!userHasPermission($db, "show_all_dcs_attachments")) {
+        $SQLdcs = 'SELECT dcs.orderno as dcno FROM dcs INNER JOIN salescase
+        ON dcs.salescaseref=salescase.salescaseref INNER JOIN salesman
+        ON salescase.salesman=salesman.salesmanname
+        
+		WHERE ( salesman.salesmanname ="' . $_SESSION['UsersRealName'] . '"
+		OR salesman.salesmancode IN  (SELECT salescase_permissions.user 
+		FROM salescase_permissions WHERE salescase_permissions.can_access = "' . $_SESSION['UserID'] . '"' . '))
+		AND dcs.orddate >= "' . $from . '"' . '
+		AND dcs.orddate <= "' . $to . '"' . '
+		';
+    } else {
+        $SQLdcs = 'SELECT dcs.orderno as dcno FROM dcs
+		WHERE dcs.orddate >= "' . $from . '"' . '
+		AND dcs.orddate <= "' . $to . '"' . '
+		';
+    }
+    $resdcs = mysqli_query($db, $SQLdcs);
+
+    $polist = [];
+
+    $POFilePath = glob("../" . $_SESSION['part_pics_dir'] . '/' . 'PurchaseOrder_*.pdf');
+
+    $couriersliplist = [];
+
+    $CourierSlipFilePath = glob("../" . $_SESSION['part_pics_dir'] . '/' . 'CourierSlip_*.pdf');
+
+    $grblist = [];
+
+    $GRBFilePath = glob("../" . $_SESSION['part_pics_dir'] . '/' . 'GRB_*.pdf');
+
+    $invoicelist = [];
+
+    $InvoiceFilePath = glob("../" . $_SESSION['part_pics_dir'] . '/' . 'Invoice_*.pdf');
+
+    $commercialinvoicelist = [];
+
+    $CommercialInvoiceFilePath = glob("../" . $_SESSION['part_pics_dir'] . '/' . 'CommercialInvoice_*.pdf');
+
+    while ($rowdcs = mysqli_fetch_assoc($resdcs)) {
+        $PO = "";
+        $ind = 0;
+        foreach ($POFilePath as $POFile) {
+
+
+
+            if (strpos($POFile, $rowdcs['dcno']) != False) {
+
+                $ind++;
+                if (strpos($POFile, "deleted") != False)
+                    $PO .= '<br /><a target = "_blank" class="btn btn-danger col-md-12" style="margin:5px 0" href = "' . $RootPath . '/' . $POFile . '">Attachment' . $ind . "</a>";
+
+                else
+                    $PO .= '<br /><a target = "_blank" class="btn btn-primary col-md-12" style="margin:5px 0" href = "' . $RootPath . '/' . $POFile . '">Attachment' . $ind . "</a> <input type='button' id='removeFile' data-basepath='" . $POFile . "' data-orderno='" . $rowdcs['dcno'] . "' class='btn btn-danger removeFile' name='removeFile' value='X'>";
+            }
+            $polist[$rowdcs['dcno']] = "<form id='attachmentForm' action='' enctype='multipart/form-data' method='post'>
+            <input type='hidden' name='FormID' value=' " . $_SESSION['FormID'] . " ' />
+            <input type='file' id='attachPOFile" . $rowdcs['dcno'] . "' data-orderno='" . $rowdcs['dcno'] . "' class='attachPOFile' name='PO'>
+            <input type='button' id='uploadPOFile' data-orderno='" . $rowdcs['dcno'] . "' class='uploadPOFile' name='uploadPOFile' value='uploadPO'>
+            </form>" . $PO;
+        }
+        $CourierSlip = "";
+        $ind = 0;
+        foreach ($CourierSlipFilePath as $CourierSlipFile) {
+
+
+
+            if (strpos($CourierSlipFile, $rowdcs['dcno']) != False) {
+
+                $ind++;
+                if (strpos($CourierSlipFile, "deleted") != False)
+                    $CourierSlip .= '<br /><a target = "_blank" class="btn btn-danger col-md-12" style="margin:5px 0" href = "' . $RootPath . '/' . $CourierSlipFile . '">Attachment' . $ind . "</a>";
+
+                else
+                    $CourierSlip .= '<br /><a target = "_blank" class="btn btn-primary col-md-12" style="margin:5px 0" href = "' . $RootPath . '/' . $CourierSlipFile . '">Attachment' . $ind . "</a> <input type='button' id='removeFile' data-basepath='" . $CourierSlipFile . "'  data-orderno='" . $rowdcs['dcno'] . "' class='btn btn-danger removeFile' name='removeFile' value='X'>";
             }
         }
+        $couriersliplist[$rowdcs['dcno']] = "<form id='attachmentForm' action='' enctype='multipart/form-data' method='post'>
+            <input type='hidden' name='FormID' value=' " . $_SESSION['FormID'] . " ' />
+            <input type='file' id='attachCourierSlipFile" . $rowdcs['dcno'] . "' data-orderno='" . $rowdcs['dcno'] . "' class='attachCourierSlipFile' name='CourierSlip'>
+            <input type='button' id='uploadCourierSlipFile' data-orderno='" . $rowdcs['dcno'] . "' class='uploadCourierSlipFile' name='uploadCourierSlipFile' value='uploadCourierSlip'>
+            </form>" . $CourierSlip;
 
-        $response[] = [
-            'dcno' => $dcno,
-            'date' => $rowDcs['date'],
-            'salescaseref' => $rowDcs['salescaseref'],
-            'client' => $rowDcs['client'],
-            'dba' => $rowDcs['dba'],
-            'invoicegroupid' => $rowDcs['invoicegroupid'],
-            'dcstatus' => $rowDcs['dcstatus'],
-            'sendtoinvoice' => empty($rowDcs['dcstatus']) ? "<input type='checkbox' class='booked' name='booked' value=1>" : $rowDcs['dcstatus'],
-            'dcinvoiced' => empty($rowDcs['dcstatus']) ? "<input type='checkbox' class='booked' name='booked' value=1>" : $rowDcs['dcstatus'],
-            'polist' => generateForm('PO', $dcno),
-            'couriersliplist' => generateForm('CourierSlip', $dcno),
-            'oldgrblist' => generateForm('GRB', $dcno),
-            'oldinvoicelist' => generateForm('Invoice', $dcno),
-            'oldcommercialinvoicelist' => generateForm('CommercialInvoice', $dcno),
-            'attachments' => $attachments
-        ];
+        $Invoice = "";
+        $ind = 0;
+        foreach ($InvoiceFilePath as $InvoiceFile) {
+
+
+
+            if (strpos($InvoiceFile, $rowdcs['dcno']) != False) {
+
+                $ind++;
+                if (strpos($InvoiceFile, "deleted") != False)
+                    $Invoice .= '<br /><a target = "_blank" class="btn btn-danger col-md-12" style="margin:5px 0" href = "' . $RootPath . '/' . $InvoiceFile . '">Attachment' . $ind . "</a>";
+
+                else
+                    $Invoice .= '<br /><a target = "_blank" class="btn btn-primary col-md-12" style="margin:5px 0" href = "' . $RootPath . '/' . $InvoiceFile . '">Attachment' . $ind . "</a> <input type='button' id='removeFile' data-basepath='" . $InvoiceFile . "'  data-orderno='" . $rowdcs['dcno'] . "' class='btn btn-danger removeFile' name='removeFile' value='X'>";
+            }
+        }
+        $invoicelist[$rowdcs['dcno']] = "<form id='attachmentForm' action='' enctype='multipart/form-data' method='post'>
+            <input type='hidden' name='FormID' value=' " . $_SESSION['FormID'] . " ' />
+            <input type='file' id='attachInvoiceFile" . $rowdcs['dcno'] . "' data-orderno='" . $rowdcs['dcno'] . "' class='attachInvoiceFile' name='Invoice'>
+            <input type='button' id='uploadInvoiceFile' data-orderno='" . $rowdcs['dcno'] . "' class='uploadInvoiceFile' name='uploadInvoiceFile' value='uploadInvoice'>
+            </form>" . $Invoice;
+
+
+        $CommercialInvoice = "";
+        $ind = 0;
+        foreach ($CommercialInvoiceFilePath as $CommercialInvoiceFile) {
+
+
+
+            if (strpos($CommercialInvoiceFile, $rowdcs['dcno']) != False) {
+
+                $ind++;
+                if (strpos($CommercialInvoiceFile, "deleted") != False)
+                    $CommercialInvoice .= '<br /><a target = "_blank" class="btn btn-danger col-md-12" style="margin:5px 0" href = "' . $RootPath . '/' . $CommercialInvoiceFile . '">Attachment' . $ind . "</a>";
+
+                else
+                    $CommercialInvoice .= '<br /><a target = "_blank" class="btn btn-primary col-md-12" style="margin:5px 0" href = "' . $RootPath . '/' . $CommercialInvoiceFile . '">Attachment' . $ind . "</a> <input type='button' id='removeFile' data-basepath='" . $CommercialInvoiceFile . "'  data-orderno='" . $rowdcs['dcno'] . "' class='btn btn-danger removeFile' name='removeFile' value='X'>";
+            }
+            /*                $commercialinvoicelist[$rowdcs['dcno']] = "<form id='attachmentForm' action='' enctype='multipart/form-data' method='post'>
+            <input type='hidden' name='FormID' value=' " . $_SESSION['FormID'] . " ' />
+            <input type='file' id='attachInvoiceFile" . $rowdcs['dcno'] . "' data-orderno='" . $rowdcs['dcno'] . "' class='attachInvoiceFile' name='Invoice'>
+            <input type='button' id='uploadInvoiceFile' data-orderno='" . $rowdcs['dcno'] . "' class='uploadInvoiceFile' name='uploadInvoiceFile' value='uploadInvoice'>
+            </form>" . $CommercialInvoice;*/
+        }
+        $commercialinvoicelist[$rowdcs['dcno']] = "<form id='attachmentForm' action='' enctype='multipart/form-data' method='post'>
+            <input type='hidden' name='FormID' value=' " . $_SESSION['FormID'] . " ' />
+            <input type='file' id='attachCommercialInvoiceFile" . $rowdcs['dcno'] . "' data-orderno='" . $rowdcs['dcno'] . "' class='attachCommercialInvoiceFile' name='CommercialInvoice'>
+            <input type='button' id='uploadCommercialnvoiceFile' data-orderno='" . $rowdcs['dcno'] . "' class='uploadCommercialInvoiceFile' name='uploadCommercialInvoiceFile' value='uploadCommercialInvoice'>
+            </form>" . $CommercialInvoice;
+
+        $GRB = "";
+        $ind = 0;
+        foreach ($GRBFilePath as $GRBFile) {
+
+
+
+            if (strpos($GRBFile, $rowdcs['dcno']) != False) {
+
+                $ind++;
+                if (strpos($GRBFile, "deleted") != False)
+                    $GRB .= '<br /><a target = "_blank" class="btn btn-danger col-md-12" style="margin:5px 0" href = "' . $RootPath . '/' . $GRBFile . '">Attachment' . $ind . "</a>";
+
+                else
+                    $GRB .= '<br /><a target = "_blank" class="btn btn-primary col-md-12" style="margin:5px 0" href = "' . $RootPath . '/' . $GRBFile . '">Attachment' . $ind . "</a> <input type='button' id='removeFile' data-basepath='" . $GRBFile . "'  data-orderno='" . $rowdcs['dcno'] . "' class='btn btn-danger removeFile' name='removeFile' value='X'>";
+            }
+        }
+        $grblist[$rowdcs['dcno']] = "<form id='attachmentForm' action='' enctype='multipart/form-data' method='post'>
+            <input type='hidden' name='FormID' value=' " . $_SESSION['FormID'] . " ' />
+            <input type='file' id='attachGRBFile" . $rowdcs['dcno'] . "' data-orderno='" . $rowdcs['dcno'] . "' class='attachGRBFile' name='GRB'>
+            <input type='button' id='uploadGRBFile' data-orderno='" . $rowdcs['dcno'] . "' class='uploadGRBFile' name='uploadGRBFile' value='uploadGRB'>
+            </form>" . $GRB;
     }
 
-    echo json_encode($response);
+
+    if (!userHasPermission($db, "show_all_dcs_attachments")) {
+        $SQL = 'SELECT dcs.orderno as dcno,dcs.orddate as date,dcs.salescaseref,debtorsmaster.name as client
+        ,debtorsmaster.dba,dcs.invoicegroupid,
+        dcs.dcstatus FROM dcs INNER JOIN debtorsmaster on debtorsmaster.debtorno=dcs.debtorno
+		WHERE dcs.orderno IN (SELECT dcs.orderno as dcno FROM dcs 
+		INNER JOIN salescase ON dcs.salescaseref=salescase.salescaseref
+		 INNER JOIN www_users ON salescase.salesman = www_users.realname
+		  WHERE www_users.userid
+		   IN (SELECT salescase_permissions.can_access FROM salescase_permissions
+		    WHERE salescase_permissions.user = "' . $_SESSION['UserID'] . '"' . ')) AND
+		dcs.orddate >= "' . $from . '"' . '
+		AND dcs.orddate <= "' . $to . '"' . '
+		';
+    } else {
+        $SQL = 'SELECT dcs.orderno as dcno,dcs.orddate as date,dcs.salescaseref,debtorsmaster.name as client,debtorsmaster.dba,dcs.invoicegroupid,
+        dcs.dcstatus FROM dcs INNER JOIN debtorsmaster on debtorsmaster.debtorno=dcs.debtorno
+		WHERE dcs.orddate >= "' . $from . '"' . '
+		AND dcs.orddate <= "' . $to . '"' . '
+		';
+    }
+    $res = mysqli_query($db, $SQL);
+    $response = [];
+
+
+    while ($row = mysqli_fetch_assoc($res)) {
+
+        $response[$row['dcno']] = $row;
+
+
+        $response[$row['dcno']]['grblist'] = $grbarray[$row['dcno']];
+        $response[$row['dcno']]['invoicelist'] = $invoicearray[$row['dcno']];
+
+        $checked = (($row['dcstatus'] != "") ? $row['dcstatus'] : "");
+
+        $response[$row['dcno']]['sendtoinvoice'] = (($checked == "") ? "
+                <input type='checkbox' id='booked' data-orderno='" . $row['id'] . "' class='booked' name='booked' $checked value=1>
+            
+                " : $row['dcstatus']);
+
+        $checked = (($row['dcstatus'] != "") ? $row['dcstatus'] : "");
+
+        $response[$row['dcno']]['dcinvoiced'] = (($checked == "") ? "
+                <input type='checkbox' id='booked' data-orderno='" . $row['id'] . "' class='booked' name='booked' $checked value=1>
+                
+                " : $row['dcstatus']);
+        $response[$row['dcno']]['polist'] = $polist[$row['dcno']];
+        $response[$row['dcno']]['couriersliplist'] = $couriersliplist[$row['dcno']];
+        $response[$row['dcno']]['oldgrblist'] = $grblist[$row['dcno']];
+        $response[$row['dcno']]['oldinvoicelist'] = $invoicelist[$row['dcno']];
+        $response[$row['dcno']]['oldcommercialinvoicelist'] = $commercialinvoicelist[$row['dcno']];
+    }
+    $data = [];
+    foreach ($response as $key => $value) {
+        $data[] = $value;
+    }
+
+    echo json_encode($data);
     return;
 }
-
-function generateForm($type, $dcno) {
-    return "<form id='attachmentForm' action='' enctype='multipart/form-data' method='post'>
-                <input type='hidden' name='FormID' value='" . htmlspecialchars($_SESSION['FormID']) . "' />
-                <input type='file' id='attach" . htmlspecialchars($type) . "File" . htmlspecialchars($dcno) . "' data-orderno='" . htmlspecialchars($dcno) . "' class='attach" . htmlspecialchars($type) . "File' name='" . htmlspecialchars($type) . "'>
-                <input type='button' id='upload" . htmlspecialchars($type) . "File' data-orderno='" . htmlspecialchars($dcno) . "' class='upload" . htmlspecialchars($type) . "File' name='upload" . htmlspecialchars($type) . "File' value='upload" . htmlspecialchars($type) . "'>
-            </form>";
-}
-
 
 include_once("includes/header.php");
 include_once("includes/sidebar.php");
@@ -471,7 +635,6 @@ include_once("includes/footer.php");
                 to,
                 FormID
             }, function(res, status) {
-            console.log(res);
                 res = JSON.parse(res);
                 table.rows.add(res).draw();
             });
